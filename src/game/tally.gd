12688f2892d9ab84812a75bf376e02c496f58876
SourLemon <lemon4sour@gmail.com>
2025-01-11 21:02:39 +0300

    blank functions

--- src/game/tally.gd
+++ src/game/tally.gd
@@ -2,33 +2,41 @@ extends Node2D
 
 var result: int = 0
 var round: int = 1
+var currentround = round_nums[round - 1]
 
 @onready var placer: Placer = $"../Placer"
+
+func on_machine_insert(machine: Machine):
+	pass
+	
+func on_machine_extract(machine: Machine):
+	pass
 
 func on_start_selected():
 	result = 0
 	
 	var machines = placer.MachineQueue
-	var tmp = 1
-	
-	for i in round_nums[round - 1]:
+	var total = 0
+	for start_number in currentround:
+		total = start_number
 		for m: Machine in machines:
-			tmp = m.execute(tmp)
+			total = m.execute(total)
+		result += total
 	
-	result += tmp
+	print(result)
 
 const round_nums = [[
 	1, 1, 1
 ],
 [
-	3, 5, 7
+	2, 2, 2
 ],
 [
-	1, 33, 7
+	3, 3, 3
 ]]
 
 const round_goals = [
-	10,
-	25,
-	150
+	15,
+	30,
+	45
 ]
