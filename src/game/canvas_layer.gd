06a9390d69ccc92e00499a5441d48625e922cc33
SourLemon <lemon4sour@gmail.com>
2025-01-11 20:32:27 +0300

    revertable integration

--- src/game/canvas_layer.gd
+++ src/game/canvas_layer.gd
@@ -1,14 +1,17 @@
 extends CanvasLayer
 @onready var h_box_container: HBoxContainer = $Inventory/Scroll/HBoxContainer
-@onready var button: Button = $Run/Button
+@onready var runbutton: Button = $Run/Button
+@onready var revertbutton: Button = $Revert/Button
 signal machine_selected
 signal revert
 signal run
 
+
 func update_buttons():
 	h_box_container.clear_buttons()
 	h_box_container.list_buttons()
-	button.disabled = !InventorySingleton.startable
+	runbutton.disabled = !InventorySingleton.startable
+	revertbutton.disabled = !InventorySingleton.revertable
 
 func on_machine_selected(index: int) -> void:
 	emit_signal("machine_selected",index)
