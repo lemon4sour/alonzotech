0a5157e980a47ac16da50975543a499569797cf3
brogolem35 <brogolem35@protonmail.com>
2025-01-11 18:44:24 +0300

    Simpleton

--- src/game/canvas_layer.gd
+++ src/game/canvas_layer.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 @onready var h_box_container: HBoxContainer = $Inventory/Scroll/HBoxContainer
+@onready var button: Button = $Run/Button
 signal machine_selected
 
 func update_buttons():
@@ -8,3 +9,6 @@ func update_buttons():
 
 func on_machine_selected(index: int) -> void:
 	emit_signal("machine_selected",index)
+
+func _process(delta: float) -> void:
+	button.disabled = !InventorySingleton.startable
