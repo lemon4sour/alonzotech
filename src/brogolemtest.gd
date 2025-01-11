4912cb77c00cfc0041d971cf94252608d796b721
brogolem35 <brogolem35@protonmail.com>
2025-01-11 13:56:19 +0300

    Evdeki new

--- src/brogolemtest.gd
+++ src/brogolemtest.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var m1 = Machine.new(0)
-	var m2 = Machine.new(1)
+	var m1 = Machine.construct(0)
+	var m2 = Machine.construct(1)
 	m2.upgrade(m1)
 	assert(m1.execute(0) == 4)
 	print(m1.execute(1))
--- src/machines/machine.gd
+++ src/machines/machine.gd
@@ -8,11 +8,47 @@ var func_up: bool
 var upgraded: bool = false
 var rotat: float = 0
 
+const self_scene = preload("res://src/machines/machine.tscn")
+
 @onready var background: Sprite2D = $Background
 @onready var label: Sprite2D = $Background/Label
-@onready var hole: Sprite2D = $Node2D/Hole
+@onready var hole: Sprite2D = $HoleParent/Hole
 @onready var hole_parent: Node2D = $HoleParent
 
+
+static func construct(id: int) -> Machine:
+	var obj = self_scene.instantiate()
+	
+	match(id):
+		0:
+			obj.ops.push_back(Operator.new("a + 2",
+				func(a): 
+				return a + 2
+				))
+			obj.axis = Axis.Line
+			obj.func_up = false
+		1:
+			obj.ops.push_back(Operator.new("a x 2",
+				func(a): 
+				return a * 2
+				))
+			obj.axis = Axis.Right
+			obj.func_up = false
+		2:
+			obj.ops.push_back(Operator.new(
+			"if (a == 0) {100} else {a}",
+			func(a): 
+				if a == 0:
+					return 100
+				else:
+					return a
+				))
+			obj.axis = Axis.Left
+			obj.func_up = false
+		_:
+			printerr("Ne?", id)
+	
+	return obj
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -42,38 +78,8 @@ func upgrade(m: Machine):
 	
 
 static func rng_tier1() -> Machine:
-	return Machine.new(randi() % 3)
+	return Machine.construct(randi() % 3)
 	
-
-func _init(id: int):
-	match(id):
-		0:
-			ops.push_back(Operator.new("a + 2",
-				func(a): 
-				return a + 2
-				))
-			axis = Axis.Line
-			func_up = false
-		1:
-			ops.push_back(Operator.new("a x 2",
-				func(a): 
-				return a * 2
-				))
-			axis = Axis.Right
-			func_up = false
-		2:
-			ops.push_back(Operator.new(
-			"if (a == 0) {100} else {a}",
-			func(a): 
-				if a == 0:
-					return 100
-				else:
-					return a
-				))
-			axis = Axis.Left
-			func_up = false
-		_:
-			printerr("Ne?", id)
 
 
 enum Axis {
